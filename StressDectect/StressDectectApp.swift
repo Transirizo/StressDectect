544a597592950d19ded5013b5513dc2e5ac10fca
陈汉超 <transirizo@icloud.com>
2023-05-18 16:03:13 +0800

    Initial Commit

---
+++ StressDectect/StressDectectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressDectectApp.swift
+//  StressDectect
+//
+//  Created by DZ0400458 on 2023/5/18.
+//
+
+import SwiftUI
+
+@main
+struct StressDectectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
